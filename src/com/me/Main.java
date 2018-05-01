c7210000b8196f3a1e41af2c57361db4057eb852
patrickatcw <pj8392@yahoo.com>
2018-05-01 10:54:51 -0400

    got results in main, now back over to scopecheck

--- src/com/me/Main.java
+++ src/com/me/Main.java
@@ -1,8 +1,30 @@
 package com.me;
+/*
+maybe think of scope as working from in to out
+ */
 
 public class Main {
 
     public static void main(String[] args) {
+
+        //step 5 variable, we have 2 called privateVar
+        String privateVar = "this is private to main()";
+
+        //step 6 instance
+        ScopeCheck scopeInstance = new ScopeCheck();
+        System.out.println("scopeInstance privateVar is " + scopeInstance.getPrivateVar());
+        System.out.println(privateVar);
+
+        //step 7 run
+
+        /*
+        result; note, program displays privateVar in main
+        ScopeCheck created, publicVar = 0: privateVar = 1
+        scopeInstance privateVar is 1
+        this is private to main()
+         */
+
+        //step 8 back to scopecheck to create a new method
 
     }
 
